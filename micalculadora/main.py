6b08a1808cbd5a76707a332627fcd829d373021c
Salvador <93989623+SalvadorCT@users.noreply.github.com>
2023-10-12 23:48:39 -0500

    Delete micalculadora directory

--- micalculadora/main.py
+++
@@ -1,7 +0,0 @@
-from calculadora import menu_calculadora
-
-if __name__ == "__main__":
-    print("-"*48)
-    print("Bienvenido a la Calculadora Matemática en Python")
-    print("-"*48)
-    menu_calculadora()
